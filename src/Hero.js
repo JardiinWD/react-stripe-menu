02f8674b1a471bf011c1a55ba1e99b39f469b67d
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-10-26 07:22:55 +0200

    #3 - Created and implemented the Hero component

--- src/Hero.js
+++ src/Hero.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import phoneImg from './images/phone.svg'
 import { useGlobalContext } from './context'
 
 const Hero = () => {
-  const data = useGlobalContext()
-  console.log("This is the Global context called in Hero.js");
-  console.log(data);
+  const { closeSubmenu } = useGlobalContext()
 
-  return <h2>hero component</h2>
+  return (
+    <Fragment>
+      {/* hero */}
+      <section className="hero">
+        {/* hero-center */}
+        <div className="hero-center">
+          {/* hero-info */}
+          <article className="hero-info">
+            <h1>Payments infrastructure for the internet</h1>
+            <p>
+              Millions of companies of all sizes-from startups to Fortune 500s use
+              Stripe's software and APIs to accept payments, send payouts and manage
+              their businesses online.
+            </p>
+            <button className="btn">Start Now</button>
+          </article>
+          {/* hero-images */}
+          <article className="hero-images">
+            <img src={phoneImg} alt="Hero Img" />
+          </article>
+        </div>
+      </section>
+    </Fragment>
+  )
 }
 
 export default Hero
